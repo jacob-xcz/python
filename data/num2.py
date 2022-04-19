fd5aae38c04b0d6ced91278e6b1213d45bb336ac
jacob <17689458313@163.com>
2022-04-19 22:23:54 +0800

    data/docs-pdf/

---
+++ data/num2.py
@@ -0,0 +1,18 @@
+#价格
+from unicodedata import name
+
+
+price = 8.5
+#定义购买重量
+weight = 10
+
+money = price * weight
+print(money-5)
+
+name = '小明'
+age = 18
+sex = '男'
+shengao = 172
+tizhong = 55
+#type(age)
+data = input('请输入一个数字：')
